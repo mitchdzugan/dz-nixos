226ffb2880b3cfffc32f3c4b56ed5ce217b983ff
Mitch Dzugan <mitchdzugan@gmail.com>
2024-10-30 12:16:27 -0500

    fix script

--- setConfig.sh
+++ setConfig.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then
 fi
 
 dirname=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-finalDir="$dirname/etc/nixos"
+finalDir="/etc/nixos"
 baseDir="$dirname/nixos"
 baseLength=$(( ${#baseDir} + 1 ))
 
@@ -14,12 +14,13 @@ function copyFiles() {
 	for file in "$1"/*; do
 		relfile=${file:$baseLength}
 		if [[ -d $file ]]; then
-			mkdir -p "$finalDir/$relfile"
+			sudo mkdir -p "$finalDir/$relfile"
 			copyFiles $file
 		elif [[ $file == *.esh ]]; then
-			cp <($eshBin $file) "$finalDir/${relfile%.*}"
+			cp <($eshBin $file) $dirname/eshout
+			sudo cp $dirname/eshout "$finalDir/${relfile%.*}"
 		else
-			cp $file "$finalDir/$relfile"
+			sudo cp $file "$finalDir/$relfile"
 		fi
 	done
 }
