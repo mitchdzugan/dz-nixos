268065f29b6a76c5576ea5496870ef56140c61de
Mitch Dzugan <mitchdzugan@gmail.com>
2024-11-25 15:00:34 -0600

    cava config in home-manager

--- config/configuration.nix
+++ config/configuration.nix
@@ -474,6 +474,28 @@ ACTION=="change", SUBSYSTEM=="drm", RUN+="${pkgs.autorandr}/bin/autorandr -c"
         themeFile = "purpurite";
       };
 
+      cava = {
+        enable = true;
+        settings = {
+          input = {
+            method = "pulse";
+            source = "auto";
+          };
+          color = {
+            gradient = 1;
+            gradient_count = 8;
+            gradient_color_1 = "#59cc33";
+            gradient_color_2 = "#80cc33";
+            gradient_color_3 = "#a6cc33";
+            gradient_color_4 = "#cccc33";
+            gradient_color_5 = "#cca633";
+            gradient_color_6 = "#cc8033";
+            gradient_color_7 = "#cc5933";
+            gradient_color_8 = "#cc3333";
+          };
+        };
+      };
+
       git = {
         enable = true;
         userName  = "Mitch Dzugan";
