14d8433e6691e9f81ebeeeda0a2edc68bce85c06
Mitch Dzugan <mitchdzugan@gmail.com>
2024-11-29 14:09:31 -0600

    revert to SUPER for now

--- config/configuration.nix
+++ config/configuration.nix
@@ -936,7 +936,7 @@ ACTION=="change", SUBSYSTEM=="drm", RUN+="${pkgs.autorandr}/bin/autorandr -c"
             natural_scroll = true;
           };
         };
-        "$mod" = "ALT";
+        "$mod" = "SUPER";
         bind = [
           "$mod, 36, exec, kitty"
           "$mod, W, exec, firefox"
